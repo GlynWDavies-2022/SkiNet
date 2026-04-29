e431147a6ebdcd5e751b510baed34b693fa87f4c
Glyn Davies <glynwdavies@gmail.com>
2026-04-29 17:38:40 +0100

    Created Product table.

--- SkiNet.Infrastructure/Configuration/ProductConfiguration.cs
+++ SkiNet.Infrastructure/Configuration/ProductConfiguration.cs
@@ -8,6 +8,26 @@ public class ProductConfiguration : IEntityTypeConfiguration<Product>
 {
     public void Configure(EntityTypeBuilder<Product> builder)
     {
+        builder.Property(p => p.Name)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(p => p.Description)
+            .IsRequired()
+            .HasMaxLength(100);
+
         builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
+
+        builder.Property(p => p.PictureUrl)
+            .IsRequired()
+            .HasMaxLength(255);
+
+        builder.Property(p => p.Type)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(p => p.Brand)
+            .IsRequired()
+            .HasMaxLength(50);
     }
 }
