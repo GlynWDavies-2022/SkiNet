c96078490be6aa9581f26c0dc443ae06936e9f01
Glyn Davies <glynwdavies@gmail.com>
2026-04-29 18:08:07 +0100

    Added partial Products Controller.

---
+++ SkiNet.API/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SkiNet.Core.Entities;
+using SkiNet.Infrastructure.Data;
+
+namespace SkiNet.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly SkiNetContext _context;
+
+        public ProductsController(SkiNetContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+            _context.Products.Add(product);
+
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductBy(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound($"Product with id {id} could not be found!");
+            }
+
+            return Ok(product);
+        }
+    }
+}
